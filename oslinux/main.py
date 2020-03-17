6dced4bbf09b44e6636786cbe5f31c5754c6159f
Shad0wMazt3r <58432267+Shad0wMazt3r@users.noreply.github.com>
2020-03-17 20:03:32 +0530

    Update main.py

--- oslinux/main.py
+++ oslinux/main.py
@@ -4,15 +4,20 @@ import platform
 import time
 import datetime
 print("You are running",platform.system())
-while True:
+while platform.system().startswith("linux"):
     start = input("$had0w:")
     if start == "view":
             os.system("ls")
     elif start == "help":
         print("""
-        1.view - Shows the files and folders in the directory
-        2.clear - Clears the screen
-        3.time - Shows the date and time""")
+        1.view        - Shows the files and folders in the directory
+        2.clear       - Clears the screen
+        3.time        - Shows the date and time
+        4.run         - Runs files
+        5.make folder - makes a folder
+        6.make file   - makes a file
+        7.edit        - edit a text file
+        8.old         - goes to cmd""")
     elif start == "clear":
             os.system("clear")
     elif start == "exit":
@@ -22,6 +27,22 @@ while True:
     elif start == "run":
         fo = input("Enter filename with proper commands eg. python main.py:")
         os.system(fo)
+    elif start == "make folder":
+        name = input("Enter the name of the folder:")
+        os.system("mkdir "+name)
+    elif start == "make file":
+        filename = input("Enter the name of the file:")
+        os.system("echo “” >>"+filename)
+    elif start == "edit":
+        filenameedit = input("Enter the name of the file:")
+        os.system("nano "+filenameedit)
+    elif start == "old":
+        os.system("bash")
+    elif start == "back":
+        os.chdir("..")
+    elif start == "go to":
+        goto = input("Enter the name of folder to go to:")
+        os.chdir(goto)
     elif start == "":
         continue
     else:
