0bf9aa265da27b3330a51bbd2a4a1bee7c9c0bcf
Shad0wMazt3r <58432267+Shad0wMazt3r@users.noreply.github.com>
2020-03-17 19:58:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ print("You are running",platform.system())
 while platform.system() == "Windows":
     start = input("$had0w:")
     if start == "view":
-            os.system("dir")
+        os.system("dir")
     elif start == "help":
         print("""
-        1.view   - Shows the files and folders in the directory
-        2.clear  - Clears the screen
-        3.time   - Shows the date and time
-        4.run    - runs files""")
+        1.view        - Shows the files and folders in the directory
+        2.clear       - Clears the screen
+        3.time        - Shows the date and time
+        4.run         - Runs files
+        5.make folder - makes a folder
+        6.make file   - makes a file
+        7.edit        - edit a text file
+        8.old         - goes to cmd""")
     elif start == "clear":
-            os.system("cls")
+        os.system("cls")
     elif start == "exit":
-        print("To exit press Ctrl + C and press enter")
+       print("To exit press Ctrl + C and press enter")
     elif start == "time":
         print(datetime.datetime.now())
     elif start == "run":
@@ -25,7 +29,23 @@ while platform.system() == "Windows":
         os.system(fo)
     elif start == "":
         continue
+    elif start == "make folder":
+        name = input("Enter the name of the folder:")
+        os.system("mkdir "+name)
+    elif start == "make file":
+        filename = input("Enter the name of the file:")
+        os.system("echo “” >>"+filename)
+    elif start == "edit":
+        filenameedit = input("Enter the name of the file:")
+        os.system("notepad "+filenameedit)
+    elif start == "old":
+        os.system("cmd")
+    elif start == "back":
+        os.chdir("..")
+    elif start == "go to":
+        goto = input("Enter the name of folder to go to:")
+        os.chdir(goto)
     else:
-        print(start+" is not a command in this shell. Use help to know about the commands")
+        print(start,"is not a command in this shell. Use help to view the commands")
 while platform.system().startswith("linux"):
     os.system("cd oslinux && python3 main.py")
